d5c06b9b65477b9deba0b83939d49eaaf8803ccf
sokcheadev2001 <sokchealeang.dev@gmail.com>
2024-04-20 10:24:57 +0700

    feat: :tada: first commit

---
+++ src/components/shop/ProductList.vue
@@ -0,0 +1,3 @@
+<template>
+  <h2>All Product</h2>
+</template>
---
+++ src/views/ShopView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import ProductList from '@/components/shop/ProductList.vue'
+</script>
+<template>
+  <h1>Shop</h1>
+  <ProductList />
+</template>
