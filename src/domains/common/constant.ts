d0e59eeb5cf5e370da4f58b53b87bbdc28786008
sokcheadev2001 <sokchealeang.dev@gmail.com>
2024-04-23 21:12:04 +0700

    feat: :sparkles: add menu constant

---
+++ src/domains/common/constant.ts
@@ -0,0 +1,16 @@
+import type { MenuItem } from './interface'
+
+export const MENUS: MenuItem[] = [
+  {
+    name: 'Home',
+    url: '/'
+  },
+  {
+    name: 'Shop',
+    url: '/shop'
+  }
+]
+
+export const SOCIAL_INFO = {
+  github: 'https://github.com/sokcheadev2001/vue-store'
+}
